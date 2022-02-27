90188598c37975035a30acb3b68acdea211b2113
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2022-02-27 11:05:53 -0500

    first commit

---
+++ src/helper.jsx
@@ -0,0 +1,23 @@
+//Calcula la diferencia de años
+export function obtenerDiferenciaYear(year){
+    return new Date().getFullYear() - year;
+}
+
+//Calcula el total a pagar segun la marca
+export function calcularMarca(marca){
+    let incremento;
+    switch(marca){
+        case "europeo": incremento = 1.30; //El 30% de lo que llevemos por eso este valor 
+        break;
+        
+        case "americano": incremento = 1.15;
+        break;
+
+        case "asiatico": incremento = 1.05;
+        break;
+
+        default:
+        break;
+    }
+    return incremento;
+}
